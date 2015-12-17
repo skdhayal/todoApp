8743a3e0a9c42df55bf216a19c05fc3484eb3d48
Sushil Kumar Dhayal <s1dhayal@gmail.com>
2015-12-17 18:59:21 +0530

    Add about page and some animation effects

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-// server.js
-
-// set up ========================
 var express = require('express');
 var app = express(); // create our app w/ express
 var mongoose = require('mongoose'); // mongoose for mongodb
 var morgan = require('morgan'); // log requests to the console (express4)
 var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-
 
 
 // configuration =================
@@ -22,7 +18,6 @@ app.use(bodyParser.json({
     type: 'application/vnd.api+json'
 })); // parse application/vnd.api+json as json
 app.use(methodOverride());
-
 
 
 // define model =================
@@ -87,10 +82,12 @@ app.delete('/api/todos/:todo_id', function(req, res) {
 
 
 // application ====================================================
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
     res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
-
+app.get('/about', function(req, res) {
+    res.sendfile('./public/about.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
 
 
 // listen (start app with node server.js) ======================================
